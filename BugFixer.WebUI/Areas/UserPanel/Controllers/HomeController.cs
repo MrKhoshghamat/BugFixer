770113e5fddedad79429b8b54d1fa402f8cc2250
mr.khoshghamat <Mr.khoshghamat@gmail.com>
2022-06-29 23:42:41 +0430

    Implement UserPanelArea

---
+++ BugFixer.WebUI/Areas/UserPanel/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BugFixer.WebUI.Areas.UserPanel.Controllers
+{
+    public class HomeController : UserPanelBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BugFixer.WebUI/Areas/UserPanel/Controllers/UserPanelBaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugFixer.WebUI.Areas.UserPanel.Controllers
+{
+    [Area("UserPanel")]
+    [Authorize]
+    public class UserPanelBaseController : Controller
+    {
+        public static string SuccessMessage = "SuccessMessage";
+        public static string WarningMessage = "WarningMessage";
+        public static string InfoMessage = "InfoMessage";
+        public static string ErrorMessage = "ErrorMessage";
+    }
+}
